1ccf0496466793ef91b0b5ab546eeadcb10b0913
evajuma <evansjuma1e@gmail.com>
2025-10-13 20:33:30 +0300

    helloworld

--- C/introduction.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    printf("Hello, world!\n");
-}
-//NB:- To run a C program succesfully then every time a change is made to the program then it must be compiled.
