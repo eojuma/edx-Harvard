6300ab4f0541ad6af2f1f8442d46df58cb9e683d
evajuma <evansjuma1e@gmail.com>
2025-10-13 21:11:10 +0300

    variables

---
+++ C Introduction/variables.c
@@ -0,0 +1,10 @@
+//variables are storage locations
+ #include <stdio.h>
+ int main(){
+    int calls = 4;
+    printf("%d\n",calls);
+ }
+ //In the example int calls = 4; Calls is the variable for the integer 4
+ //Numbers:%i and %d are placeholders used interchangeably for integer formatting and %f (float) for decimal numbers
+ //Text:%c (char) for single characters and %s for strings
+ // Bool:%i for boolean values(true and false).
