9ef6bead710af9219255a23ff6fe964737b311aa
eojuma <evansjuma1e@gmail.com>
2025-10-14 16:22:08 +0000

    Tue, Oct 14, 2025, 7:22 PM +03:00

--- loops/loops.c
+++ loops/loops.c
@@ -2,11 +2,11 @@
 #include <cs50.h>
 void printbricks(int bricks);
 int main(){
-	int rows = get_int("How many rows should the pyramid have?: \n");
+	int rows = get_int("How many rows should the pyramid have?: ");
 	// printf("%i\n",rows);
 	for (int j=0;j<rows;j++){
 
-		printbricks(rows);
+		printbricks(j+1);
 	}
 }
 void printbricks(int bricks){
@@ -14,4 +14,5 @@ void printbricks(int bricks){
 
 		printf("#");
 	}
+	printf("\n");
 }
