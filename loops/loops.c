582403fedfb6f1c49b5c4bd1b900b1d24748ef0f
eojuma <evansjuma1e@gmail.com>
2025-10-14 15:36:28 +0000

    Tue, Oct 14, 2025, 6:36 PM +03:00

--- loops/loops.c
+++ loops/loops.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <cs50.h>
 int main(){
-	rows = get_int("how long do you want our pyramid to be?\n");
+	int rows = get_int("how long do you want our pyramid to be?\n");
 	printf("%i\n",rows);
 }
