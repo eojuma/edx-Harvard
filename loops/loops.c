4d5f69fa4a5b83f5d53ba369dc9aa996bf06df5a
eojuma <evansjuma1e@gmail.com>
2025-10-14 16:17:55 +0000

    Tue, Oct 14, 2025, 7:17 PM +03:00

--- loops/loops.c
+++ loops/loops.c
@@ -4,7 +4,10 @@ void printbricks(int bricks);
 int main(){
 	int rows = get_int("How many rows should the pyramid have?: \n");
 	// printf("%i\n",rows);
-	printbricks(rows);
+	for (int j=0;j<rows;j++){
+
+		printbricks(rows);
+	}
 }
 void printbricks(int bricks){
 	for (int i = 0 ;i<bricks;i++){
