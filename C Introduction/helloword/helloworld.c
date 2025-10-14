c46e399813e17253c3975882578aa2f47dedd6e0
evajuma <evansjuma1e@gmail.com>
2025-10-14 09:37:28 +0300

    Added libcs50 as a submodule

---
+++ C Introduction/helloword/helloworld.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main()
+{
+    printf("Hello, world!\n");
+    string greeting = get_string("What is your name?: ");
+    printf("My name is %s\n", greeting);
+    int age = get_int("What is your age %s: ", greeting);
+    printf("I am %d years old\n", age);
+}
+// NB:- To run a C program succesfully,every time a change is made to the program then it must be compiled.
