6eda86e21fa9816f1da9d4d1d3e8a62751ed4afd
eojuma <evansjuma1e@gmail.com>
2025-10-15 07:10:34 +0000

    Wed, Oct 15, 2025, 10:10 AM +03:00

--- loops/assignment.c
+++ loops/assignment.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <cs50.h>
-void printbricks(int bricks);
+void printbricks(int bricks, string chr);
 int main(){
     int rows = get_int("Number of rows: ");
     // printf("%d",rows);
@@ -14,7 +14,7 @@ int main(){
 void printbricks(int bricks, string chr){
     for (int i=0;i<bricks;i++){
 
-        printf(chr);
+        printf("%s",chr);
     }
     printf("\n");
 }
