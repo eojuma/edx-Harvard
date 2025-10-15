df4be6280fe3ece68234d24eed8277942d13550d
eojuma <evansjuma1e@gmail.com>
2025-10-15 07:09:05 +0000

    Wed, Oct 15, 2025, 10:09 AM +03:00

--- loops/assignment.c
+++ loops/assignment.c
@@ -4,16 +4,17 @@ void printbricks(int bricks);
 int main(){
     int rows = get_int("Number of rows: ");
     // printf("%d",rows);
-    for (int j=0;j<rows;j++){
+    for (int j=0;j<=rows;j++){
 //   int space=rows-1;
-        printbricks(j);
+        printbricks(rows-j, " ");
+        printbricks(j, "#");
     }
 
 }
-void printbricks(int bricks){
+void printbricks(int bricks, string chr){
     for (int i=0;i<bricks;i++){
 
-        printf("#");
+        printf(chr);
     }
     printf("\n");
 }
