16eef4a88b276265b3911eb808e32486e99e55e6
eojuma <evansjuma1e@gmail.com>
2025-10-16 15:51:05 +0000

    Thu, Oct 16, 2025, 6:51 PM +03:00

--- assignment/validtriange.c
+++ assignment/validtriange.c
@@ -6,9 +6,10 @@ int main(void){
 }
 bool validtriangle(int a,int b,int c){
     if (a+b+c)<1{
-        return false
+        return false;
     }
     if ((a+b)>c || (a+c)>b || (c+b)>a){
-        
+        return false;
     }
+    
 }
