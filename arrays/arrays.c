f1601f3e644e8bd1bc0823301f2be3542f9afcca
eojuma <evansjuma1e@gmail.com>
2025-10-16 05:01:43 +0000

    Thu, Oct 16, 2025, 8:01 AM +03:00

---
+++ arrays/arrays.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+int main(void){
+
+    // Simple array creation
+    int num[3];
+    num[0]=2;
+    num[1]=3;
+    num[2]=4;
+    int sum=0;
+    for (i=0;i<strlen(num);i++){
+        sum+=num[i]
+    }
+}
