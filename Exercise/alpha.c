7fdb74956ab77c2a5488aac439382a2e95de7658
eojuma <evansjuma1e@gmail.com>
2025-10-16 10:18:48 +0000

    Thu, Oct 16, 2025, 1:18 PM +03:00

---
+++ Exercise/alpha.c
@@ -0,0 +1,13 @@
+// Write a program to check if the charaters of a word given by the user are in alphabetical order and print out ye otherwise no
+#include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+int main(void){
+string str=get_string("Word: ");
+for (int i=0;i<strlen(str);i++){
+    if str[i]>str[i+1]{
+        printf("no\n");
+        return 0;
+    }
+}
+}
