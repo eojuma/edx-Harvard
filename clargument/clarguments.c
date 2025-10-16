0e4741f69fb43697f8af549f67c88c25551d1cd2
eojuma <evansjuma1e@gmail.com>
2025-10-16 11:39:32 +0000

    Thu, Oct 16, 2025, 2:39 PM +03:00

--- clargument/clarguments.c
+++ clargument/clarguments.c
@@ -3,6 +3,10 @@
 #include <string.h>
 
 int main(int argc, string argv[]){
+    if (argc !=2){
+        printf("Provide a valid argument\n");
+        return 1;
+    }
     string str=argv[1];
     int len=strlen(str);
     for (int i=0;i<len-1;i++){
