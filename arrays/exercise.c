33ab28af164f069fcad513e2cb0a4a9d38dca14d
eojuma <evansjuma1e@gmail.com>
2025-10-16 08:51:08 +0300

    exercise

--- arrays/exercise.c
+++ arrays/exercise.c
@@ -1,9 +1,12 @@
+//Create an array of size five with the first element of one(1) and the subsequent elements of double digits
+
 #include <cs50.h>
 #include <stdio.h>
 
 int main(void){
     int array[5]={1,2,4,8,16};
     for (int i=0;i<(sizeof(array)/sizeof(array[0]));i++){
-        printf("%d%d%d%d%d",array[0],array[1],array[2],array[3],array[4]);
+        printf("%d ",array[i]);
     }
+    printf("\n");
 }
