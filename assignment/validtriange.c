1a67af52b9fff7e1584425bbfc353403bca439ce
eojuma <evansjuma1e@gmail.com>
2025-10-16 16:00:23 +0000

    Thu, Oct 16, 2025, 7:00 PM +03:00

--- assignment/validtriange.c
+++ assignment/validtriange.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+#include <cs50.h>
 bool validtriangle(int a,int b,int c);
 int main(void){
-printf("%i\n",validtriangle(2,4,1))
+printf("%i\n",validtriangle(2,4,1));
 
 }
 bool validtriangle(int a,int b,int c){
-    if (a+b+c)<1{
+    if (a+b+c)<1){
         return false;
     }else if ((a+b)>c || (a+c)>b || (c+b)>a){
         return false;
