1c6f8b889b7cd49957f8667538bb38a572b788b0
eojuma <evansjuma1e@gmail.com>
2025-10-16 11:10:18 +0000

    Thu, Oct 16, 2025, 2:10 PM +03:00

--- clargument/clarguments.c
+++ clargument/clarguments.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 #include <cs50.h>
+#include <string.h>
 
 int main(int argc, string argv[]){
     string str=argv[1];
-    len=strlen(str)
+    int len=strlen(str);
     for (int i=0;i<len-1;i++){
         if (str[i]<str[i+1]){
-            printf("Yes\n")
-            return 0
+            printf("Yes\n");
+            return 0;
         }
     }
-    printf("No\n")
+    printf("No\n");
 }
