d0b4f8f6097377cfeaca982a8073e1dbc60507ce
eojuma <evansjuma1e@gmail.com>
2025-10-16 15:31:37 +0000

    Thu, Oct 16, 2025, 6:31 PM +03:00

--- Functions/functions.c
+++ Functions/functions.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
 #include <cs50.h>
 int add(int a, int b);
+float mult(float a,float b);
 int main(void){
 printf("%d\n",add(2,5));
+printf("%f",mult(2,5));
 }
 
 int add(int a,int b){
     return a+b;
+}
+
+float mult(float a,float b){
+    return a*b;
 }
