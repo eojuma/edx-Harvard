8c195984ac5ddd4d95dd0ba217b2dd4e871074a9
eojuma <evansjuma1e@gmail.com>
2025-10-17 17:16:29 +0000

    Fri, Oct 17, 2025, 8:16 PM +03:00

---
+++ structs/structs.go
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <cs50.h>
+#iclude <strings.h>
+
+
+typedef struct{
+	string name;
+	string number;
+}person;
+
+int main(void){
+
+	person people[3]{
+		people[0].name="Evans Juma"
+		people[0].number="0720787683"
+
+
+		people[1].name="TomLee Osbon"
+		people[1].number="0700565647"
+
+		people[2].name="Kevin Wesonga"
+		people[2].number="0707152873"
+
+	}
+	string name=get_string("Employee name: \n")
+	int len = strln(name)
+	for (int i=0;i<len;i++){
+		if (strcmp(name,person.name[1])==0){
+			printf("%s and the number is % s",name,number)
+		}
+	}
+}
