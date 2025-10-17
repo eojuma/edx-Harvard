4906051aec26f4e59ba2bb950658ea7fa313ed57
eojuma <evansjuma1e@gmail.com>
2025-10-17 17:18:45 +0000

    Fri, Oct 17, 2025, 8:18 PM +03:00

--- structs/structs.go
+++ structs/structs.go
@@ -11,22 +11,24 @@ typedef struct{
 int main(void){
 
 	person people[3]{
-		people[0].name="Evans Juma"
-		people[0].number="0720787683"
+		people[0].name="Evans Juma";
+		people[0].number="0720787683";
 
 
-		people[1].name="TomLee Osbon"
-		people[1].number="0700565647"
+		people[1].name="TomLee Osbon";
+		people[1].number="0700565647";
 
-		people[2].name="Kevin Wesonga"
-		people[2].number="0707152873"
+		people[2].name="Kevin Wesonga";
+		people[2].number="0707152873";
 
 	}
-	string name=get_string("Employee name: \n")
-	int len = strln(name)
+	string name=get_string("Employee name: \n");
+	int len = strln(name);
 	for (int i=0;i<len;i++){
 		if (strcmp(name,person.name[1])==0){
-			printf("%s and the number is % s",name,number)
+			printf("%s and the number is %s \n",name,number);
+			return 0;
 		}
 	}
+	printf("Not found\n")
 }
