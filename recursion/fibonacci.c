c770d5dcf036591f2f6f25bd8058042d5a079882
eojuma <evansjuma1e@gmail.com>
2025-10-21 15:19:50 +0000

    Tue, Oct 21, 2025, 6:19 PM +03:00

---
+++ recursion/fibonacci.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int fibo(int num);
+int main(void){
+int num=get_int("NUmber: ");
+printf("%d\n",fibo(num));
+}
+
+int fibo(int n){
+if (n==0){
+    return 1;
+}
+
+if (n==1){
+    return 1;
+}
+
+    return fibo(n-1)+fibo(n-2);
+
+}
+
