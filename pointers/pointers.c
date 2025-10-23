e754c56527642c186035899976bad0dcd82397e1
eojuma <evansjuma1e@gmail.com>
2025-10-23 09:11:05 +0000

    Thu, Oct 23, 2025, 12:11 PM +03:00

--- pointers/pointers.c
+++ pointers/pointers.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main(void){
-    int calls = 4;
-    int *p=&calls;
-    printf("%p\n",&calls);  // or 
+    int calls = 4;  //calls holds 4
+    int *p=&calls;  //p holds the address of calls  and *p gives you the value stored at that adrees which is 4
+    printf("%p\n",&calls);  // or  printf("%p\n",p);
     printf("%d\n",*p);
 }
